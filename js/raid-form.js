a67da22574051d9a7204b5090b51100fdbb8cf5c
rorduno <rorduno88@gmail.com>
2017-03-29 11:53:27 -0500

    poc

--- js/raid-form.js
+++ js/raid-form.js
@@ -1,21 +1,19 @@
 $(document).ready(function () {
-	// this is the id of the form
-	$("#raid-form").submit(function(e) {
-		var url = 'https://sheets.googleapis.com/v4/spreadsheets/1WgBRT7onjhSzouNPcEdIvcb_Ae0Js7C3OOxXzVpAvhw/values/A%3AB:append?valueInputOption=USER_ENTERED&alt=json&key=AIzaSyAVgBnngVDj_8yq9LMlV5akpchmgFwEeqc';
 
-		var data = { "values": [
-	                    [ '666', '555' ]
-	                 ]
-	               }
+	$("#raid-form").submit(function(e) {
+		var url = '';
+
+		var data = {};
+
 	    $.ajax({
 	           type: "POST",
 	           url: url,
-	           data:   data, //$("#raid-form").serialize(), // serializes the form's elements.
+	           data:   data,
 	           success: function(data) {
-	               alert('success'); // show response from the php script.
+	               alert('success');
 	           }
 	         });
 
-	    e.preventDefault(); // avoid to execute the actual submit of the form.
+	    e.preventDefault();
 	});
 });
